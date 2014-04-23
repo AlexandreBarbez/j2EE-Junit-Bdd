7b8b12c64fcfaf839f59effd28f357ea34833a17
alexandre saint-orens <alexandre.saintorens@gmail.com>
2014-04-23 11:07:39 +0200

    update inscription

--- tests_unitaires/src/main/java/monMain.java
+++ tests_unitaires/src/main/java/monMain.java
@@ -4,6 +4,7 @@ import static java.lang.Math.abs;
  * Created by Alex on 22/04/2014.
  */
 public class monMain {
+	
     public static int trouverValeurAbsolue(int i) {
         return abs(i);
     }
--- tests_unitaires/src/test/java/monMainTest.java
+++ tests_unitaires/src/test/java/monMainTest.java
@@ -9,10 +9,17 @@ import static org.junit.Assert.assertEquals;
 public class monMainTest {
 
     @Test
-    public void renvoieUnNombre(){
+    public void renvoieUnNombreNegatif(){
             assertEquals(2,monMain.trouverValeurAbsolue(-2));
     }
 
+    @Test
+    public void renvoieUnNombrePositif(){
+        assertEquals(2,monMain.trouverValeurAbsolue(2));
+}
 
-
+    @Test
+    public void renvoieUnNombreZero(){
+        assertEquals(0,monMain.trouverValeurAbsolue(0));
+}
 }
